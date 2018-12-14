94618fe5e974ba21b985f4cbf25514fd649a0330
Antkeo1 <Antonio.keo@ieleadership.org>
2018-12-14 13:59:32 -0500

    created some frontend auth logic and a skelaton layout for html

---
+++ assets/scripts/ui.js
@@ -0,0 +1,79 @@
+'use strict'
+
+const store = require('./store.js')
+
+const signUpSuccess = data => {
+  store.user = data.user
+  $('#message').text('Signed up successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  $('#sign-up').trigger('reset')
+  console.log('signUpSuccess ran. Data is :', data)
+}
+
+const signUpFailure = error => {
+  $('#message').text('Error on sign up')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('signUpFailure ran. Error is :', error)
+  alert('Sign Up unsuccessful')
+}
+
+const signInSuccess = data => {
+  store.user = data.user
+  $('#message').text('Signed in successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  $('#sign-in').trigger('reset')
+  console.log('signInSuccess ran. Data is :', data)
+}
+
+const signInFailure = error => {
+  $('#message').text('Error on sign in')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('signInFailure ran. Error is :', error)
+  alert('Sign In unsuccessful')
+}
+
+const changePasswordSuccess = data => {
+  $('#message').text('Password changed successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  $('#change-password').trigger('reset')
+  console.log('changePasswordSuccess ran. Data is :', data)
+}
+
+const changePasswordFailure = error => {
+  $('#message').text('Error on password change')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('changePasswordFailure ran. Error is :', error)
+  alert('Password Change unsuccessful')
+}
+
+const signOutSuccess = data => {
+  $('#message').text('Signed out successfully')
+  store.user = null
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  console.log('signOutSuccess ran. Data is :', data)
+}
+
+const signOutFailure = error => {
+  $('#message').text('Error on sign out')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('signOutFailure ran. Error is :', error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure,
+}
