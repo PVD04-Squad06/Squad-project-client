332ac79637570f9213792927b0cf7b831894fbb5
rakshyakc <rakshya.khatri1@gmail.com>
2018-12-20 13:13:06 -0500

    Added multiple products and prices to the shopping cart modal

--- assets/scripts/browse/events.js
+++ assets/scripts/browse/events.js
@@ -4,47 +4,28 @@ const api = require('./api.js')
 const ui = require('./ui.js')
 const store = require('../store.js')
 
-// Only needed to test front end/back end connection.
-// const onGetAllProducts = event => {
-//   event.preventDefault()
-//   console.log('onGetAllProducts', event.target)
-//   api.getAllProducts()
-//     .then(ui.getAllProductsSuccess)
-//     .catch(ui.getAllProductsFailure)
-// }
-
-const onGetAProduct = event => {
-  event.preventDefault()
-  console.log(event.target)
-  api.getAProduct()
-    .then(ui.getAProductSuccess)
-    .catch(ui.getAProductFailure)
-}
-
 const onAddToCart = event => {
   event.preventDefault()
   const productIdToBuy = event.target.id
   // $('#shopping-cart').html('')
   const dataArray = store.womensProduct
-  console.log(store.womensProduct)
-  let content = '<div class="row">'
+  // console.log(store.womensProduct)
+  let content = '<table>'
   dataArray.forEach(function (product) {
     // const addToCart = $('.')
     if (productIdToBuy === product._id) {
-      content += `<div class="col-sm-4">
-        <div class="card-body" id="imageDivalso">
-          <p class="card-text">${product.name}</p>
-          <p class="card-text">$${product.price}</p>
-        <div>
-      </div>`
+      content += `<tr>
+          <td class="shopTable">${product.name}</td>
+          <td class="shopTable">$${product.price}</td>
+      </tr>`
     }
     // <button class="btn btn-dark add-to-cart" id="${product._id}">Add to cart</button>
     // store.product._id = product
     // console.log(product)
     // console.log(product.image)
   })
-  content += '</div>'
-  $('#shopping-cart-list').html(content)
+  content += '</table>'
+  $('#shopping-cart-list').append(content)
   console.log(content)
 }
 // console.log(event.target.id)
@@ -77,8 +58,24 @@ const onGetMensClothing = event => {
     .then(ui.getMensClothingSuccess)
 }
 
+// Only needed to test front end/back end connection.
+// const onGetAllProducts = event => {
+//   event.preventDefault()
+//   console.log('onGetAllProducts', event.target)
+//   api.getAllProducts()
+//     .then(ui.getAllProductsSuccess)
+//     .catch(ui.getAllProductsFailure)
+// }
+
+// const onGetAProduct = event => {
+//   event.preventDefault()
+//   console.log(event.target)
+//   api.getAProduct()
+//     .then(ui.getAProductSuccess)
+//     .catch(ui.getAProductFailure)
+// }
+
 module.exports = {
-  onGetAProduct,
   onAddToCart,
   onGetWomensClothing,
   onGetMensClothing,
